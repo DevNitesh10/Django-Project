2e467a1e5bdc246fc3825a643c99ddf025ae986e
DevNitesh10 <princenitesh325@gmail.com>
2023-07-18 08:53:26 +0545

    Read Operation done Data added from admin panel

--- crud/views.py
+++ crud/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Blog #Objects is Manager here by default
 
 # Create your views here.
 def index(request):
-    # return HttpResponse("Hello, Sabin! You're at the polls index.")
-    return render(request,"crud/index.html")
+    blog = Blog.objects.all()
+    return render(request,"crud/index.html", {"blogs" : blog})
 
 def about(request):
     return render(request,"crud/about.html")
