21035435342050914f98e7d77432284f6ceb8df8
whoisnitesh <104932931+DevNitesh10@users.noreply.github.com>
2023-07-25 09:39:14 +0545

    Delete vege directory

--- vege/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Receipe(models.Model): #models is a manager here
-    receipe_name = models.CharField(max_length=50)
-    receipe_description = models.TextField()
-    receipe_image = models.ImageField(upload_to="Receipe_Image")
